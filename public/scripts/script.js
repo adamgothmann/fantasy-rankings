c79b899e22f18559f84d90be3cc18f0452795b38
adamgothmann <adamgothmann@gmail.com>
2016-09-21 19:10:45 -0500

    api working with angular http call

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,35 +1,41 @@
 var allPlayers;
 
-$(document).ready(function(){
-  console.log('something');
-  $.ajax({
-      url: "https://api.fantasydata.net/nfl/v2/JSON/FantasyPlayers?Team=PIT" ,
-      beforeSend: function(xhrObj){
-          // Request headers
-          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","630f356526af4b368fe08040535f6dda");
-      },
-      type: "GET",
-      // Request body
-      // data: "{body}",
-      success: function(data){
-        allPlayers = data;
-        console.log(allPlayers);
-        // for(var i = 0; i < 200; i++){
-        //
-        // }
-      }
-  })
-  .done(function(data) {
-    alert("success");
-  })
-  .fail(function() {
-    alert("error");
-  });
-});
+// $(document).ready(function(){
+//   console.log('something');
+//   $.ajax({
+//     url: "https://api.fantasydata.net/nfl/v2/JSON/FantasyPlayers?Team=PIT" ,
+//     beforeSend: function(xhrObj){
+//         // Request headers
+//         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","630f356526af4b368fe08040535f6dda");
+//     },
+//     type: "GET",
+//     success: function(data){
+//       allPlayers = data;
+//       console.log(allPlayers);
+//       // for(var i = 0; i < 200; i++){
+//       //
+//       // }
+//     }
+//   })
+//   .done(function(data) {
+//     alert("success");
+//   })
+//   .fail(function() {
+//     alert("error");
+//   });
+// });
 
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
   console.log("in angular");
   console.log(allPlayers);
+  $http({
+    url: "https://api.fantasydata.net/nfl/v2/JSON/FantasyPlayers?Team=PIT",
+    headers: {"Ocp-Apim-Subscription-Key": "630f356526af4b368fe08040535f6dda"},
+    method: 'GET'
+  }).then(function(response){
+    allPlayers = response.data;
+    console.log(allPlayers);
+  });
 }]);
