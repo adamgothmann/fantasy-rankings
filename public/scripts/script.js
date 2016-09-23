e7713c623417ac516d7f78658744f7a65517acb4
adamgothmann <adamgothmann@gmail.com>
2016-09-23 15:16:34 -0500

    angular

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,35 +1,7 @@
-var allPlayers;
-
-// $(document).ready(function(){
-//   console.log('something');
-//   $.ajax({
-//     url: "https://api.fantasydata.net/nfl/v2/JSON/FantasyPlayers?Team=PIT" ,
-//     beforeSend: function(xhrObj){
-//         // Request headers
-//         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","630f356526af4b368fe08040535f6dda");
-//     },
-//     type: "GET",
-//     success: function(data){
-//       allPlayers = data;
-//       console.log(allPlayers);
-//       // for(var i = 0; i < 200; i++){
-//       //
-//       // }
-//     }
-//   })
-//   .done(function(data) {
-//     alert("success");
-//   })
-//   .fail(function() {
-//     alert("error");
-//   });
-// });
-
-var myApp = angular.module('myApp', []);
+var myApp=angular.module( 'myApp', [] );
 
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
-  console.log("in angular");
-  console.log(allPlayers);
+  var allPlayers;
   $http({
     url: "https://api.fantasydata.net/nfl/v2/JSON/FantasyPlayers?Team=PIT",
     headers: {"Ocp-Apim-Subscription-Key": "630f356526af4b368fe08040535f6dda"},
@@ -37,5 +9,8 @@ myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
   }).then(function(response){
     allPlayers = response.data;
     console.log(allPlayers);
+    console.log(allPlayers[2].Position);
   });
+  $scope.name = 'something';
+  console.log($scope.name);
 }]);
